f555f78e3bfd1f7b48a85945f00df3baf498c9e0
dimzragil <kiritotamvan48@gmail.com>
2022-05-17 21:47:10 +0700

    idk

--- application/controllers/formulir.php
+++ application/controllers/formulir.php
@@ -16,5 +16,34 @@ class Formulir extends CI_Controller
 
         $this->load->view('user/formulirH', $data);
         $this->load->view('user/formulir', $data);
+
+        $formulir = [
+            'program' => $this->input->post('program'),
+            'nama' => $this->input->post(''),
+            'no_id' => $this->input->post('no_id'),
+            'nama_ayah' => $this->input->post('nama_ayah'),
+            'tempat_lahir' => $this->input->post('tempat_lahir'),
+            'tanggal_lahir' => $this->input->post('tanggal_lahir'),
+            'no_paspor' => $this->input->post('no_paspor'),
+            'keluar_paspor' => $this->input->post('keluar_paspor'),
+            'tg_keluar_paspor' => $this->input->post('tg_keluar_paspor'),
+            'masa_berlaku_paspor' => $this->input->post('masa_berlaku_paspor'),
+            'jenis_kelamin' => $this->input->post('jenis_kelamin'),
+            'goldar' => $this->input->post('goldar'),
+            'status_perkawinan' => $this->input->post('status_perkawinan'),
+            'alamat' => $this->input->post('alamat'),
+            'email' => $this->input->post('email'),
+            'telp_rumah' => $this->input->post('telp_rumah'),
+            'telp_hp' => $this->input->post('telp_hp'),
+            'pendidikan' => $this->input->post('pendidikan'),
+            'pekerjaan' => $this->input->post('pekerjaan'),
+            'keluarga_ikut_umroh' => $this->input->post('keluarga_ikut_umroh'),
+            'nomor_keluarga_ikut_umroh' => $this->input->post('nomor_keluarga_ikut_umroh'),
+            'informasi_pendaftaran' => $this->input->post('informasi_pendaftaran'),
+            'penyakit' => $this->input->post('penyakit'),
+            'keluarga_darurat' => $this->input->post('keluarga_darurat'),
+            'nomor_keluarga_darurat' => $this->input->post('nomor_keluarga_darurat'),
+            'alamat_keluarga_darurat' => $this->input->post('alamat_keluarga_darurat'),
+        ];
     }
 }
