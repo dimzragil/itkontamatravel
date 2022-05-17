267fb0f35621c1170c8277f9431c309d1096423d
Avozyz <dzafif95@gmail.com>
2022-05-17 10:36:18 +0700

    ganti login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -13,7 +13,7 @@
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Logan</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Login</h1>
                                     </div>
                                     <?= $this->session->flashdata('message'); ?>
                                     <form class="user" method="POST" action="<?= base_url('auth'); ?>">
