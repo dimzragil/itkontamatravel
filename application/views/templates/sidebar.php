ff34e2b5fb0e905ee576d6379aef84e19884f303
dimzragil <kiritotamvan48@gmail.com>
2022-06-08 11:11:48 +0700

    p

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -6,7 +6,7 @@
         <div class="sidebar-brand-icon rotate">
             <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">ADMIN ITKONTAMA TRAVEL</div>
+        <div class="sidebar-brand-text mx-3"></div>
     </a>
 
     <!-- Divider -->
